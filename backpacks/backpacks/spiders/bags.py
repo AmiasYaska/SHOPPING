f92b50dcb72dd0fa1f62bfad09bb11cbc299a485
Amias Yaska <amiasyaska@gmail.com>
2024-03-07 20:15:05 +0300

    scraped 5 pages

--- backpacks/backpacks/spiders/bags.py
+++ backpacks/backpacks/spiders/bags.py
@@ -7,13 +7,19 @@ class BagsSpider(scrapy.Spider):
     name = "bags"
     allowed_domains = ["target.com"]
 
+    MAX_PAGES = 5
+
     def start_requests(self):
-        yield scrapy.Request(
-            # url="https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v2?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&channel=WEB&count=24&default_purchasability_filter=true&include_dmc_dmr=true&include_sponsored=true&keyword=backpacks&new_search=true&offset=72&page=%2Fs%2Fbackpacks&platform=desktop&pricing_store_id=1771&scheduled_delivery_store_id=1771&store_ids=1771%2C1768%2C1113%2C3374%2C1792&useragent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F122.0.0.0+Safari%2F537.36&visitor_id=018CB60F6F0E0201B85AB692A144B57D&zip=52404",
-            url="https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v2?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&channel=WEB&count=24&default_purchasability_filter=true&include_dmc_dmr=true&include_sponsored=true&keyword=backpacks&new_search=true&offset=0&page=%2Fs%2Fbackpacks&platform=desktop&pricing_store_id=1771&store_ids=1771%2C1768%2C1113%2C3374%2C1792&useragent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F122.0.0.0+Safari%2F537.36&visitor_id=018CB60F6F0E0201B85AB692A144B57D&zip=52404",
-            method="GET",
-            callback=self.parse
-        )
+        for page_num in range(self.MAX_PAGES):
+            offset = page_num * 24
+            # url = f"https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v2?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&channel=WEB&count=24&default_purchasability_filter=true&include_dmc_dmr=true&include_sponsored=true&keyword=backpacks&new_search=true&offset={offset}&page=%2Fs%2Fbackpacks&platform=desktop&pricing_store_id=1771&store_ids=1771%2C1768%2C1113%2C3374%2C1792&useragent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F122.0.0.0+Safari%2F537.36&visitor_id=018CB60F6F0E0201B85AB692A144B57D&zip=52404",
+            url = f"https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v2?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&channel=WEB&count=24&default_purchasability_filter=true&include_dmc_dmr=true&include_sponsored=true&keyword=backpacks&new_search=true&offset={offset}&page=%2Fs%2Fbackpacks&platform=desktop&pricing_store_id=1771&store_ids=1771%2C1768%2C1113%2C3374%2C1792&useragent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36&visitor_id=018CB60F6F0E0201B85AB692A144B57D&zip=52404"
+
+            yield scrapy.Request(
+                url,
+                method="GET",
+                callback=self.parse
+            )
 
     def parse(self, response):
         data = json.loads(response.body)
